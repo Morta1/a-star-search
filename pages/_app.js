7682ed08fe8e0a14f091db85ea70643255c1b503
Mor <mortaob@gmail.com>
2021-09-23 08:55:55 +0300

    added all content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: Roboto, serif;
+  }
+`
+const theme = {};
+
+const MyApp = ({ Component, pageProps }) => {
+  return <ThemeProvider theme={theme}>
+    <GlobalStyle/>
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
 
 export default MyApp
